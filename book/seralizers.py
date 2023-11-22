8161e68a3801dc253eb279b9688a2439d3feea1d
MOHAMED MAHER <141060515+MohamedMaherTawfik@users.noreply.github.com>
2023-11-22 17:34:45 +0200

    add serializers

--- book/seralizers.py
+++ book/seralizers.py
@@ -1,20 +1,19 @@
 from rest_framework import serializers
 from .models import Book,Author,Review
 
-
+#book serializers
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model= Book
         fields = '__all__' 
 
-
-
+#author serializers
 class AuthorSerializers(serializers.ModelSerializer):
     class Meta:
         model=Author
         fields= '__all__'
         
-        
+#review serializers      
 class RewviewSerializer(serializers.ModelSerializer):
     class Meta:
         model=Review 
